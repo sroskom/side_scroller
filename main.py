99d0a70e89c7ee2843d505745c94cff99f853282
sroskom <sterlingroskom@gmail.com>
2017-06-26 21:58:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
-from lib.main import CusWid
+from lib import CusWid
 from kivy.core.audio import SoundLoader
 from os.path import join
+import sys
 
 class Manager(ScreenManager):
     
@@ -12,28 +13,39 @@ class Manager(ScreenManager):
         self.startCampMusic()
         
     def startScrollerMusic(self, *args):
-        self.scrolrSound = SoundLoader.load(join(App.get_running_app().user_data_dir,'background_song_01.mp3'))
-        if self.scrolrSound:
-            self.scrolrSound.loop = True
-            self.scrolrSound.play()
-            
+        try:
+            self.scrolrSound = SoundLoader.load(join(App.get_running_app().user_data_dir,'background_song_01.mp3'))
+            if self.scrolrSound:
+                self.scrolrSound.loop = True
+                self.scrolrSound.play()
+        except:
+            pass
+        
     def startCampMusic(self, *args):
-        self.campSound = SoundLoader.load(join(App.get_running_app().user_data_dir,'camp_song.mp3'))
-        if self.campSound:
-            self.campSound.loop = True
-            self.campSound.play()
-
+        try:
+            self.campSound = SoundLoader.load(join(App.get_running_app().user_data_dir,'camp_song.mp3'))
+            if self.campSound:
+                self.campSound.loop = True
+                self.campSound.play()
+        except:
+            pass
+        
     def stopCampMusic(self, *args):
-        if self.campSound and self.campSound.state != 'stop':
-            print(self.campSound.state)
-            self.campSound.stop()
-            self.campSound.unload()
+        try:
+            if self.campSound and self.campSound.state != 'stop':
+                print(self.campSound.state)
+                self.campSound.stop()
+                self.campSound.unload()
+        except:
+            pass
 
     def stopScrollerMusic(self, *args):
-        if self.scrolrSound and self.scrolrSound.state != 'stop':
-            self.scrolrSound.stop()
-            self.scrolrSound.unload()
-        
+        try:
+            if self.scrolrSound and self.scrolrSound.state != 'stop':
+                self.scrolrSound.stop()
+                self.scrolrSound.unload()
+        except:
+            pass
 class TempApp(App):
     def on_pause(self, *args):
         return True
@@ -42,6 +54,11 @@ class TempApp(App):
         return manager
 
 if __name__ == '__main__':
-    app = TempApp()
-    app.run()
-    
+    try:
+        app = TempApp()
+        app.run()
+    except:
+        e = sys.exc_info()
+        f = open('/sdcard/temp/exceptions.txt', 'w')
+        f.write(str(e))
+        f.close()
